a71162d553385be71f96b7a6e3669f8a284ace2e
ghadakhaldi <khaldikhaldighada@gmail.com>
2021-11-30 14:30:23 +0100

    second commit

---
+++ src/component/profile/FullName.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function FullName() {
+    return (
+        <div>
+            <h1> Ghada khaldi</h1>
+        </div>
+    )
+}
+
+export default FullName
---
+++ src/component/profile/ProfilePhoto.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import image from "../../img/img2.png"
+function ProfilePhoto() {
+    return (
+        <div>
+    <img src={image} />
+        </div>
+    )
+}
+
+export default ProfilePhoto
